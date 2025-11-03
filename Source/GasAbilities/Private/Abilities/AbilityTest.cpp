a1d6933ae16826bf1424f9d21ae8a859833a61fd
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-03 17:16:23 +0100

    feat: add a test ability

---
+++ Source/GasAbilities/Private/Abilities/AbilityTest.cpp
@@ -0,0 +1,22 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#include "Abilities/AbilityTest.h"
+
+void UAbilityTest::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+									const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (ActorInfo->AvatarActor.Get())
+	{
+		// Do the ability
+		UE_LOG(LogTemp, Display, TEXT("Active Ability"));
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("Ability Activated"));
+
+		const FVector OwnerLocation = ActorInfo->AvatarActor.Get()->GetActorLocation();
+		GetWorld()->SpawnActor<AActor>(ActorToSpawn, OwnerLocation, FRotator::ZeroRotator);
+	}
+
+	// End the ability
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
---
+++ Source/GasAbilities/Public/Abilities/AbilityTest.h
@@ -0,0 +1,24 @@
+﻿// GasAbilities © 2025 Fabio Pittaccio. All Rights Reserved. Personal, non-commercial use only. See LICENSE and README for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "AbilityTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASABILITIES_API UAbilityTest : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+								 const FGameplayAbilityActivationInfo ActivationInfo,
+								 const FGameplayEventData* TriggerEventData) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> ActorToSpawn;
+};
