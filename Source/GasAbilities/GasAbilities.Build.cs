e44566c7fdb30776ad2432b539fc4e5dbc9e37ed
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-05 11:31:48 +0100

    feat: add Gameplay Ability System dependencies to project configuration

--- Source/GasAbilities/GasAbilities.Build.cs
+++ Source/GasAbilities/GasAbilities.Build.cs
@@ -8,16 +8,14 @@ public class GasAbilities : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", // Essential Unreal Engine modules
+			"InputCore", "EnhancedInput", // Input handling modules
+			"GameplayAbilities", "GameplayTags", "GameplayTasks" // Gameplay Ability System modules
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
