29d213d46ae86b8b1999f831744dcb739f5f050b
Fabio Pittaccio <pittacciofabio@gmail.com>
2025-11-11 17:45:56 +0100

    refractor: separate learning content to a specific folder

--- Source/GasAbilities/Private/00_Learning/GameMode/GasGameModeAttributes.cpp
+++ Source/GasAbilities/Private/00_Learning/GameMode/GasGameModeAttributes.cpp
@@ -7,7 +7,7 @@ AGasGameModeAttributes::AGasGameModeAttributes()
 {
 	// Set the default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT(
-		"/Game/Blueprints/Characters/BP_CharacterAttributes"));
+		"/Game/00_Learning/Blueprints/Characters/BP_CharacterAttributes"));
 
 	// If the Blueprint class is found, set it as the default pawn class
 	if (PlayerPawnBPClass.Class != nullptr)
@@ -17,7 +17,7 @@ AGasGameModeAttributes::AGasGameModeAttributes()
 
 	// Set the default player controller class to our custom controller
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT(
-		"/Game/Blueprints/Controllers/BP_ControllerAttributes"));
+		"/Game/00_Learning/Blueprints/Controllers/BP_ControllerAttributes"));
 
 	// If the Blueprint class is found, set it as the default player controller class
 	if (PlayerControllerBPClass.Class != nullptr)
